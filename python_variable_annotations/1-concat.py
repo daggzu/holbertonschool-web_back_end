a85bf87e54d8f6994d36209c4871390743fb6a21
daggzu <diegog8603@gmail.com>
2024-01-04 13:01:40 -0400

    okkkk lets go

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -5,7 +5,7 @@ arguments and returns a concatenated string
 """
 
 def concat (str1:str, str2:str) -> str:
-    """ function that returns concatanated string"""
+    """ function that returns concatenated string"""
     return str1 + str2
     
 
