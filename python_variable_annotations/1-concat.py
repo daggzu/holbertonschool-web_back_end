07ce057857eb87f4061eae8290e6b269d601232b
daggzu <diegog8603@gmail.com>
2024-01-04 13:00:29 -0400

    okaytyyy lets go

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/user/bin/env python3
+"""
+Module creates a type-annotated function concat that takes a string str1 and a string str2 as
+arguments and returns a concatenated string
+"""
+
+def concat (str1:str, str2:str) -> str:
+    """ function that returns concatanated string"""
+    return str1 + str2
+    
+
+
