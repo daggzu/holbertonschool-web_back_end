3d134d317b4d2310358d979c360be9edc9b8a951
daggzu <diegog8603@gmail.com>
2024-01-04 13:28:47 -0400

    our love died off slowly

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Module creates type-annotated function that takes a float argument
+and returns string representation of the float."""
+def to_str(n:float) -> str:
+    """function that returns a string rep of float"""
+    return str(n)
