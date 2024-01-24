926b9fd04e82dde88826f3cbafbfba08bd47c8e5
daggzu <diegog8603@gmail.com>
2024-01-24 17:54:52 -0400

    idek

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,24 +1,38 @@
 /* eslint-disable */
 export default class Currency {
-    constructor(code, name){
-        this._code = code;
-        this._name = name;
-
+    constructor(code, name) {
+        if (typeof code !== 'string') throw TypeError('Code must be a string'); {
+            this._code = code;
+        }
+        if (typeof name !== 'string') throw TypeError('Name must be a string'); {
+            this._name = name;
+        }
     }
-get code() {
-    return this._code;
-}
-set code(newCode) {
-    this._code = newCode;
-}
-get name() {
-    return this._name;
-}
-set name(newName) {
-    this._name = newName;
-}
 
-displayFullCurrency() {
-    return '${this._name} (${this._code})';
-}
+
+    get code() {
+        return this._code;
+    }
+
+    set code(newCode) {
+        if (typeof newCode !== 'string') throw TypeError('Code must be a string'); {
+            this._code = newCode;
+        }
+    }
+
+
+    get name() {
+        return this._name;
+    }
+
+    set name(newName) {
+        if (typeof newName !== 'string') throw TypeError('Name must be a string'); {
+            this._name = newName;
+        }
+    }
+
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+    }
 }
