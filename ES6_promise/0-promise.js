4f6c3ba09f9560f58cf9402fd7ccf11dce39b1b3
daggzu <diegog8603@gmail.com>
2024-01-26 11:28:28 -0400

    max verstaoppen

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('true');
+        reject(new Error('API'));
+      });
+    });
+  }
+  export default getResponseFromAPI;
