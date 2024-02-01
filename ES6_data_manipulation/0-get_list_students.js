f220c0a9910b183a5c81069bb9f67aa243e867a1
daggzu <diegog8603@gmail.com>
2024-02-01 17:07:30 -0400

    oopsie

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -4,6 +4,6 @@ export default function getListsStudents(){
         {id: 1, firstname: "Guillaume", location: "San Francisco"},
         {id: 2, firstname: "James", location: "Columbia"},
         {id: 5, firstname: "Serena", location: "San Francisco"},
-    ]
+    ];
 }
 
