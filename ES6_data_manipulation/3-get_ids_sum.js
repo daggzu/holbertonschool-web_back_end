c209977f91a7ce55578140301f92689fea6be2a8
daggzu <diegog8603@gmail.com>
2024-02-01 18:06:15 -0400

    real time

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export default function getStudentIdsSum(students) {
+    if (!Array.isArray(students)) {
+      return [];
+    }
+    return students.reduce((accumulator, student) => accumulator + student.id, 0);
+  }
