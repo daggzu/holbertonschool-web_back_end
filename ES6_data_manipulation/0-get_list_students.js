a020cbfb5b6bef3e3226d79e1307afade7ed2cba
daggzu <diegog8603@gmail.com>
2024-02-01 17:04:49 -0400

    setting up project

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function getListsStudents(){
+    return [
+        {id: 1, firstname: "Guillaume", location: "San Francisco"},
+        {id: 2, firstname: "James", location: "Columbia"},
+        {id: 5, firstname: "Serena", location: "San Francisco"},
+    ]
+}
+
